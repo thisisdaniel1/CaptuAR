94ef3a9441c92df20100cad22cbcc72986ab6b8d
Daniel Nguyen <dknguye5@uno.edu>
2023-09-11 12:09:35 -0500

    Locates lat & long using expo-location
    Prints lat & long of a location to console. Currently not the correct location and cannot modify the circle to be drawn around those lat & long but still some progress.
    
    See link for more on expo-location:
    https://docs.expo.dev/versions/latest/sdk/location/?utm_source=google&utm_medium=cpc&utm_content=performancemax&gclid=Cj0KCQjw9fqnBhDSARIsAHlcQYQcPCH4zPU_47zphx2F4uJRemJNg6sOa5Dv0n4mf6Uul8-A0-VgSWgaAirOEALw_wcB

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,29 +1,65 @@
-import React, { useContext } from "react";
-import { Text, View, StyleSheet, ActivityIndicator } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, PLatform } from "react-native";
 import MapView, {Polyline, Circle} from "react-native-maps";
+import * as Location from "expo-location";
 
 const HomeScreen = () => {
 
-  return <View style={styles.container}>
-        <MapView 
-            style={styles.map}
-            initialRegion={{
-              latitude: 37.78825,
-              longitude: -122.4324,
-              latitudeDelta: 0.01,
-              longitudeDelta: 0.01,
-            }}
-        >
-          <Circle
-            center={{
-              latitude: 37.78825,
-              longitude: -122.4324,}}
-              radius={200}
-              strokeColor="rgba(158,158,255,1.0)"
-              fillColor="rgba(158,158,255,0.3)"
-          />
-        </MapView>
-      </View>
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let {status} = await Location.requestForegroundPermissionsAsync();
+      // if user has not granted permission, skip
+      if (status !== "granted"){
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = "Waiting...";
+  if (errorMsg){
+    text = errorMsg;
+  }
+  else if (location){
+    
+    const {coords} = location;
+    const {latitude, longitude} = coords;
+
+    // console.log(latitude);
+    // console.log(longitude);
+    // console.log(coords);
+    // text = JSON.stringify(location);
+  }
+
+  return  <View style={styles.container}>
+      <MapView 
+          style={styles.map}
+          initialRegion={{
+            latitude: latitude,
+            longitude: longitude,
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.01,
+          }}
+      >
+        <Circle
+          center={{
+            latitude: latitude,
+            longitude: longitude,}}
+            radius={200}
+            strokeColor="rgba(158,158,255,1.0)"
+            fillColor="rgba(158,158,255,0.3)"
+        />
+      </MapView>
+    </View>
+    // <View>
+    //   <Text>{text}</Text>
+    // </View>
   };
 
   const styles = StyleSheet.create({
